7e4c906831ff15331364c60735af4b2260cced81
kwallace3590 <kwallace3590@gmail.com>
2024-06-03 15:06:41 -0400

    3

--- vue/src/components/SearchCriteria.vue
+++ vue/src/components/SearchCriteria.vue
@@ -2,12 +2,24 @@
     <div>
         <form>
            <div>
-                <label for="city">Enter City name </label>
-                <input type="text" name="city">
+            <label for="start-address">What is your starting point?</label>
+            <input type="text" name="start-address">
+           </div>
+            <div>
+                <label for="distance">How many miles would you like to travel?</label>
+                <input type="number" name="distance" max="20" min="0" step="0.1">
             </div>
             <div>
                 <label for="date">Please select a date </label>
                 <input type="date" name="date">
+            </div>
+            <div>
+                <label for="start-time">What is your start time?</label>
+                <input type="time" name="start-time">
+            </div>
+            <div>
+                <label for="end-time">What is your end time?</label>
+                <input type="time" name="end-time">
             </div>
             
         </form>
