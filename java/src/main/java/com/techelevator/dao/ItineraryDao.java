de195d26b7fd5e917de12c8b855c4a0bb9e754a6
Sonali Panda <sonalipanda237@hotmail.com>
2024-06-03 12:23:20 -0400

    new addition

---
+++ java/src/main/java/com/techelevator/dao/ItineraryDao.java
@@ -0,0 +1,14 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Itinerary;
+
+import java.util.List;
+
+public interface ItineraryDao {
+
+   List<Itinerary> getItineraries();
+   Itinerary getMyItinerary(int itineraryId);
+    Itinerary createItinerary(Itinerary itinerary);
+    Itinerary updateItinerary(Itinerary itinerary);
+
+}
